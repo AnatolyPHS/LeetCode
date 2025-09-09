5791690103a52f16cd57d8c0e59479f6defef9cc
atol <ibragim191765@gmail.com>
2025-09-09 13:41:09 +0700

    Cycle Sort

--- LeetCode_practice/Assets/Scripts/Solution.cs
+++ LeetCode_practice/Assets/Scripts/Solution.cs
@@ -4,14 +4,17 @@ public class Solution : MonoBehaviour
 {
     private void Start()
     {
-        int[] nums = new int[] { 1, 2, 0 };
+        /*int[] nums = new int[] { 100000, 3, 4000, 2, 15, 1, 99999 };*/
+        /*int[] nums = new int[] { 1,2,0 };*/
+        /*int[] nums = new int[] { 3,4,-1,1};*/
+        int[] nums = new int[] { 0,-1,3,1};
         FirstMissingPositive(nums);
     }
     
     public int FirstMissingPositive(int[] nums) {
         int minValue = int.MaxValue;
         int maxValue = int.MinValue;
-
+        
         for (int i = 0 ; i < nums.Length; i++){
             int currentValue = nums[i];
             if (currentValue <= 0){
@@ -31,27 +34,27 @@ public class Solution : MonoBehaviour
             return 1;
         }
 
-        int minValueSecondLap = minValue;
-        int maxValueSecondLap = maxValue;
-
-        for (int i = 0 ; i < nums.Length; i++){
-            if (nums[i] <= 0){
+        int sortedIndex = 0;
+        while (sortedIndex < nums.Length)
+        {
+            int index = nums[sortedIndex] - 1;
+            if (nums[sortedIndex] > 0 && nums[sortedIndex] < nums.Length && nums[sortedIndex] != nums[index])
+            {
+                (nums[sortedIndex], nums[index]) = (nums[index], nums[sortedIndex]);
                 continue;
             }
             
-            if(nums[i] == minValueSecondLap + 1){
-                minValueSecondLap++;
-            }
-
-            if(nums[i] == maxValueSecondLap - 1){
-                minValueSecondLap--;
+            sortedIndex++;
+        }
+        
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] - nums[i - 1] != 1)
+            {
+                return nums[i -1] + 1;
             }
         }
 
-        if(maxValueSecondLap - minValueSecondLap > 1){
-            return (minValueSecondLap + 1);
-        }
-
-        return maxValueSecondLap + 1;
+        return maxValue + 1;
     }
 }
