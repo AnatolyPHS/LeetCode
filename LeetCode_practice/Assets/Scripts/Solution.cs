a8f322c1bc05f0fcb93089422d51a2ba35bce5ce
ANATPC\ibrag <anatbraging@gmail.com>
2025-09-26 12:11:56 +0700

    60. Permutation Sequence - Gemeni helped

--- LeetCode_practice/Assets/Scripts/Solution.cs
+++ LeetCode_practice/Assets/Scripts/Solution.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Solution : MonoBehaviour
@@ -8,73 +9,32 @@ public class Solution : MonoBehaviour
     }
     
     public string GetPermutation(int n, int k) {
-        int[] numbers = new int[n];
-        for (int i = 1 ; i <= n; i++){
-            numbers[i - 1] = i;
+        List<int> numbers = new List<int>();
+        for (int i = 1; i <= n; i++)
+        {
+            numbers.Add(i);
         }
-
-        int[] final =  GetPermutatedArrayAtStep(numbers, k);
-
-        string result = "";
-        for (int i = 0; i < final.Length; i++){
-            result += final[i].ToString();
-        }
-
-        return result;
-    }
-
-    private int[] GetPermutatedArrayAtStep(int[] inputArray, int step){
-        if (step == 0){
-            return inputArray;
-        }
-        step--;
-
-        int[] tmpArray = new int[inputArray.Length];
-        inputArray.CopyTo(tmpArray, 0);
         
-        int currentArrayValue = CalculateValue(tmpArray);
-        for (int i = tmpArray.Length - 1; i >= 0 ; i--){
-            for (int j = i - 1; j >= 0; j--){
-                int tmp = tmpArray[i];
-                tmpArray[i] = tmpArray[j];
-                tmpArray[j] = tmp;
-                int nextMinValue =  CalculateValue(tmpArray);
-
-                if (currentArrayValue > nextMinValue){
-                    tmp = tmpArray[i];
-                    tmpArray[i] = tmpArray[j];
-                    tmpArray[j] = tmp;
-                    continue;
-                }
-
-                return GetPermutatedArrayAtStep(tmpArray, step);
-            }
-        }
-
-        return  inputArray;
-    }
-
-    private int CalculateValue(int[] inputArray){
-        int finalSumm = 0;
-        for(int i = inputArray.Length - 1; i >= 0 ; i--){
-            finalSumm += inputArray[i] * Power(10, (inputArray.Length - i - 1));
-        }
-
-        return finalSumm;
-    }
-    
-    private int Power(int baseValue, int exponent)
-    {
-        if (exponent == 0)
+        int[] factorials = new int[n];
+        factorials[0] = 1;
+        for (int i = 1; i < n; i++)
         {
-            return 1;
+            factorials[i] = factorials[i - 1] * i;
+        }
+        
+        k--;
+        
+        string result =  "";
+        
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int index = k / factorials[i];
+            result = result + numbers[index].ToString();
+            numbers.RemoveAt(index);
+
+            k %= factorials[i];
         }
 
-        int result = 1;
-        for (int i = 0; i < exponent; i++)
-        {
-            result *= baseValue;
-        }
-        return result;
+        return result.ToString();
     }
 }
