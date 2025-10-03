6eb2b68a5713bd51c05251d8d702278854412b7c
ANATPC\ibrag <anatbraging@gmail.com>
2025-10-03 12:34:00 +0700

    68. Text Justification

--- LeetCode_practice/Assets/Scripts/Solution.cs
+++ LeetCode_practice/Assets/Scripts/Solution.cs
@@ -5,135 +5,36 @@ public class Solution : MonoBehaviour
 {    
     private void Start()
     {
-        /*IsNumber("-.E3"); // should be false*/
-        /*IsNumber("-.E3");// should be false*/
-        IsNumber("+.8");// should be true
     }
     
-private HashSet<char> digits = new HashSet<char>();
-    
-    public bool IsNumber(string s) {
-                
-        for (char c = '0'; c <= '9'; c++)
-        {
-            digits.Add(c);
-        }
+    public IList<string> FullJustify(string[] words, int maxWidth) {
+        IList<string> result = new List<string>();
+        List<string> newLine = new List<string>();
 
-        int dotPositionIndex = -1;
-        int ePositionIndex = -1;
-        int plusminusPositionIndex = -1;
-        int stringLastIndex = s.Length - 1;
-        int digitsCount = 0;
+        int charInLineNumber = 0;
+        for(int i = 0; i < words.Length; i++){
+            string word = words[i];
 
-        if (stringLastIndex == 0){
-            return digits.Contains(s[0]);
-        }
-
-        for(int i = stringLastIndex; i >= 0 ; i --){
-            char symbol = s[i];
-
-            if (digits.Contains(symbol)){
-                digitsCount++;
+            if (word.Lenght == maxWidth){
+                result.Add(word);
                 continue;
             }
 
-            if (symbol == 'e' || symbol == 'E'){
-                if (i == stringLastIndex){
-                    return false;
-                }
-
-                if (i == 0){
-                    return false;
-                }
-                
-                if (ePositionIndex != -1) {
-                    return false;
-                }
-                ePositionIndex = i;
-            }
-            else if (symbol == '-' || symbol == '+'){
-                if (i == 0){
-                    continue;
-                }
-
-                if (plusminusPositionIndex != -1){
-                    return false;
-                }
-
-                plusminusPositionIndex = i;
-            }
-            else if (symbol == '.'){
-                if (dotPositionIndex != -1){
-                    return false;
-                }
-
-                dotPositionIndex = i;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        if (digitsCount == 0){
-            return false;
-        }
-
-        return SpecialSymbolsInProperPositions(plusminusPositionIndex, dotPositionIndex, ePositionIndex, s);
-
-    }
-
-    private bool SpecialSymbolsInProperPositions(int plusminusPositionIndex,
-     int dotPositionIndex,int ePositionIndex, string s)
-    {
-        bool positionsAreFine = true;
-        int lastPosition = s.Length - 1;
-
-        if (plusminusPositionIndex != -1){
-            positionsAreFine &= plusminusPositionIndex != lastPosition && digits.Contains(s[plusminusPositionIndex + 1])
-            && ePositionIndex == plusminusPositionIndex - 1;
-        }
-        
-        bool theFirstSymbolIsSign = s[0] == '-' || s[0] == '+';
-        
-        if (dotPositionIndex != -1){
-            positionsAreFine &= (dotPositionIndex == lastPosition) || (digits.Contains(s[dotPositionIndex + 1])) 
-                                                                   || ePositionIndex == dotPositionIndex + 1;
-        }
-
-        if (theFirstSymbolIsSign && lastPosition>= 1)
-        {
-            positionsAreFine &= (digits.Contains(s[1]) || dotPositionIndex == 1);
-            if (dotPositionIndex == 1)
-            {
-                positionsAreFine &= ePositionIndex != 2;
-            }
-        }
-
-        if (dotPositionIndex == 0 && lastPosition>= 1)
-        {
-            positionsAreFine &= digits.Contains(s[1]);
-        }
-        
-        if (ePositionIndex != -1){
-            positionsAreFine &= ePositionIndex != 0;
-            positionsAreFine &= ePositionIndex != lastPosition;
-            positionsAreFine &= (theFirstSymbolIsSign == false) || ePositionIndex > 1;
-
-            if (positionsAreFine == false)
-            {
-                return false;
+            if (charInLineNumber + word.Length <= maxWidth - 1){
+                newLine.Add(word)
+                charInLineNumber += 1;
+                charInLineNumber += word.Length;
+                continue;
             }
 
-            bool ePatterIsFine = (digits.Contains(s[ePositionIndex + 1]) ||
-                                  (ePositionIndex + 1 == plusminusPositionIndex &&
-                                   plusminusPositionIndex != lastPosition &&
-                                   digits.Contains(s[plusminusPositionIndex + 1])));
-            
-            positionsAreFine &= ePatterIsFine;
-            positionsAreFine &= ePositionIndex > dotPositionIndex;
+            result.Add(ConvertListToWord(newLine));
+            charInLineNumber = word.Length;
+            newLine.Clear()
+            newLine.Add(word);
         }
 
-        return positionsAreFine;
+        ProcessLastLine(result);
+
+        return result;
     }
 }
