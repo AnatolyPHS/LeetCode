46d268e408cece88bf3c4c24d58b156f7f7e33b2
ANATPC\ibrag <anatbraging@gmail.com>
2025-10-06 11:56:15 +0700

    68. Text Justification

--- LeetCode_practice/Assets/Scripts/Solution.cs
+++ LeetCode_practice/Assets/Scripts/Solution.cs
@@ -6,35 +6,63 @@ public class Solution : MonoBehaviour
     private void Start()
     {
     }
+
+    private int maxWordWidth = -1;
     
     public IList<string> FullJustify(string[] words, int maxWidth) {
         IList<string> result = new List<string>();
         List<string> newLine = new List<string>();
 
+        maxWordWidth = maxWidth;
+
         int charInLineNumber = 0;
+        int numberOfSymbols = 0;
         for(int i = 0; i < words.Length; i++){
             string word = words[i];
 
-            if (word.Lenght == maxWidth){
+            if (word.Length == maxWidth){
                 result.Add(word);
                 continue;
             }
-
-            if (charInLineNumber + word.Length <= maxWidth - 1){
-                newLine.Add(word)
+            
+            if (charInLineNumber + word.Length <= maxWidth - 1)
+            {
+                newLine.Add(word);
                 charInLineNumber += 1;
                 charInLineNumber += word.Length;
+                numberOfSymbols += word.Length;
                 continue;
             }
 
-            result.Add(ConvertListToWord(newLine));
+            result.Add(ConvertListToWord(newLine,numberOfSymbols));
             charInLineNumber = word.Length;
-            newLine.Clear()
+            numberOfSymbols = 0;
+            newLine.Clear();
             newLine.Add(word);
         }
 
         ProcessLastLine(result);
 
         return result;
+    }
+
+    private string ConvertListToWord(List<string> newLine, int wordsLength)
+    {
+        string finalWord = ''; // need a builder
+        int spacesToAdd = maxWordWidth - wordsLength;
+        int spacesNeeded = newLine.Count - 1;
+        int spacesPerPosition = spacesToAdd / spacesNeeded;
+        int excessiveSpaces = spacesToAdd % spacesNeeded;
+
+        for (int i = 0; i < newLine.Count; i++)
+        {
+            finalWord = finalWord + newLine[0];
+            string onlySpaces = new string(' ', spacesPerPosition  + excessiveSpaces > 0 ? 1 : 0);
+            excessiveSpaces--;
+        }
+        
+        
+        
+        return finalWord;
     }
 }
