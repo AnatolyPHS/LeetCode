b6e41471cd037013f28bbc2617b4c018b493246e
ANATPC\ibrag <anatbraging@gmail.com>
2025-10-08 12:30:21 +0700

    68. Text Justification

--- LeetCode_practice/Assets/Scripts/Solution.cs
+++ LeetCode_practice/Assets/Scripts/Solution.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,8 +8,8 @@ public class Solution : MonoBehaviour
 {    
     private void Start()
     {
-        /*string[] words = {"This", "is", "an", "example", "of", "text", "justification."};*/
-        string[] words = {"What","must","be","acknowledgment","shall","be"};
+        string[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+        /*string[] words = {"What","must","be","acknowledgment","shall","be"};*/
         int maxWidth = 16;
         IList<string> result = FullJustify(words, maxWidth);
         foreach( string line in result)
@@ -29,19 +30,21 @@ public class Solution : MonoBehaviour
         int numberOfSymbols = 0;
         for(int i = 0; i < words.Length; i++){
             string word = words[i];
+            bool isLastWord = i == words.Length - 1;
 
             if (word.Length == maxWidth){
                 result.Add(word);
                 continue;
             }
             
-            if (charInLineNumber + word.Length <= maxWidth - 1)
+            if (charInLineNumber + (newLine.Count - 1)+ word.Length <= maxWidth - 1)
             {
                 newLine.Add(word);
-                charInLineNumber += 1;
-                charInLineNumber += word.Length;
                 numberOfSymbols += word.Length;
-                continue;
+                if (isLastWord == false)
+                {
+                    continue;
+                }
             }
 
             result.Add(ConvertListToWord(newLine,numberOfSymbols));
@@ -49,20 +52,24 @@ public class Solution : MonoBehaviour
             numberOfSymbols = 0;
             newLine.Clear();
             newLine.Add(word);
+
+            if (isLastWord)
+            {
+                result.Add(ConvertListToWord(newLine,numberOfSymbols)); 
+            }
         }
-
-        ProcessLastLine(result.Last());
-
+        
+        result[^1] = ProcessLastLine(result.Last());
         return result;
     }
 
-    private void ProcessLastLine(string word)
+    private string ProcessLastLine(string word)
     {
         string[] words = word.Split(' ');
         word = string.Join(" ", words);
         int spacesToAdd = maxWordWidth - word.Length;
         string endSpaces = new string(' ', spacesToAdd);
-        word = word + endSpaces;
+        return (word + endSpaces);
     }
 
     private string ConvertListToWord(List<string> newLine, int wordsLength)
@@ -70,8 +77,8 @@ public class Solution : MonoBehaviour
         string finalWord = string.Empty; // need a builder
         int spacesToAdd = maxWordWidth - wordsLength;
         int spacesNeeded = newLine.Count - 1;
-        int spacesPerPosition = spacesToAdd / spacesNeeded;
-        int excessiveSpaces = spacesToAdd % spacesNeeded;
+        int spacesPerPosition = spacesNeeded == 0 ? spacesToAdd : spacesToAdd / spacesNeeded;
+        int excessiveSpaces =  spacesNeeded == 0 ? 0 : spacesToAdd % spacesNeeded;
 
         for (int i = 0; i < newLine.Count; i++)
         {
