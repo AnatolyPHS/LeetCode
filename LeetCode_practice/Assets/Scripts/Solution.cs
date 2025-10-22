4b9072304c4b25ad76707826019e6e02bea53afc
ANATPC\ibrag <anatbraging@gmail.com>
2025-10-22 11:56:26 +0700

    76. Minimum Window Substring

--- LeetCode_practice/Assets/Scripts/Solution.cs
+++ LeetCode_practice/Assets/Scripts/Solution.cs
@@ -8,110 +8,62 @@ public class Solution : MonoBehaviour
 {    
     private void Start()
     {
-        /*string[] words = {"This", "is", "an", "example", "of", "text", "justification."};*/
-        /*string[] words = {"What","must","be","acknowledgment","shall","be"};*/
-        string[] words = {"What","must","be","shall","be."};
-        int maxWidth = 5;
-        IList<string> result = FullJustify(words, maxWidth);
-        foreach( string line in result)
-        {
-            Debug.Log(line);
-        }
+        MinWindow("ADOBECODEBANC", "ABC");
     }
 
-    private int maxWordWidth = -1;
-    
-    public IList<string> FullJustify(string[] words, int maxWidth) {
-        IList<string> result = new List<string>();
-        List<string> newLine = new List<string>();
-
-        maxWordWidth = maxWidth;
-
-        int charInLineNumber = 0;
-        int numberOfSymbols = 0;
-        for(int i = 0; i < words.Length; i++){
-            string word = words[i];
-            bool isLastWord = i == words.Length - 1;
-
-            if (word.Length == maxWidth){
-                if (newLine.Count > 0)
-                {
-                    result.Add(ConvertListToWord(newLine,numberOfSymbols));
-                    newLine.Clear();
-                    numberOfSymbols = 0;
-                }
-                result.Add(word);
-                continue;
-            }
-            
-            if (numberOfSymbols + (newLine.Count - 1)+ word.Length <= maxWidth - 1)
-            {
-                newLine.Add(word);
-                numberOfSymbols += word.Length;
-                word = String.Empty;
-                if (isLastWord == false)
-                {
-                    continue;
-                }
-            }
-
-            if (isLastWord)
-            {
-                result.Add(ConvertListToWord(newLine,numberOfSymbols));
-                if (string.IsNullOrEmpty(word) == false)
-                {
-                    result.Add(ConvertListToWord(new List<string>(){word},word.Length));
-                }
-                break;
-            }
-            
-            result.Add(ConvertListToWord(newLine,numberOfSymbols));
-            
-            numberOfSymbols = word.Length;
-            newLine.Clear();
-            newLine.Add(word);
-        }
-        
-        result[^1] = ProcessLastLine(result.Last());
-        return result;
-    }
-
-    private string ProcessLastLine(string word)
+    public string MinWindow(string s, string t)
     {
-        string[] words = word.Split(' ');
-        List<string> realWords = words.ToList().Where(w => w != "").ToList();
-        word = string.Join(" ", realWords);
-        int spacesToAdd = maxWordWidth - word.Length;
-        string endSpaces = new string(' ', spacesToAdd);
-        return (word + endSpaces);
+        int tLength = t.Length;
+        int sLength = s.Length;
+
+        if (tLength > sLength)
+        {
+            return "";
+        }
+
+        for (int frameLength = tLength; frameLength < sLength; frameLength++)
+        {
+            for (int i = 0; i <= s.Length - frameLength; i++)
+            {
+                string subString = s.Substring(i, frameLength);
+                if (HasAllLetters(subString, t))
+                {
+                    return subString;
+                }
+            }
+        }
+
+
+        return "";
     }
 
-    private string ConvertListToWord(List<string> newLine, int wordsLength)
+    private bool HasAllLetters(string InputString, string lettersToHave)
     {
-        string finalWord = string.Empty; // need a builder
-        int spacesToAdd = maxWordWidth - wordsLength;
-        int intermediateSpacePositionsNumber = newLine.Count - 1;
-        int spacesPerPosition = intermediateSpacePositionsNumber == 0 ? spacesToAdd : spacesToAdd / intermediateSpacePositionsNumber;
-        int excessiveSpaces =  intermediateSpacePositionsNumber == 0 ? 0 : spacesToAdd % intermediateSpacePositionsNumber;
-
-        if (newLine.Count == 1)
+        Dictionary<char, int> lettersInQueryString = new Dictionary<char, int>();
+        for (int i = 0; i < InputString.Length; i++)
         {
-            string onlySpaces = new string(' ', spacesToAdd);
-            return newLine[0] + onlySpaces;
-        }
-        
-        for (int i = 0; i < newLine.Count; i++)
-        {
-            finalWord = finalWord + newLine[i];
-            string onlySpaces = new string(' ', spacesPerPosition  + (excessiveSpaces > 0 ? 1 : 0));
-            if (i < newLine.Count - 1)
+            char letter = InputString[i];
+            if (lettersInQueryString.Keys.Contains(letter)){
+                lettersInQueryString[letter]++;
+            }
+            else
             {
-                finalWord += onlySpaces;
+                lettersInQueryString.Add(letter, 1);
+            }
+        }
+
+        for (int i = 0; i < lettersToHave.Length; i++)
+        {
+            char letterToHave = lettersToHave[i];
+            if (lettersInQueryString.Keys.Contains(letterToHave) == false 
+                || lettersInQueryString[letterToHave] <= 0)
+            {
+                return false;
             }
 
-            excessiveSpaces--;
+            lettersInQueryString[letterToHave]--;
         }
-        
-        return finalWord;
+
+        return true;
     }
 }
